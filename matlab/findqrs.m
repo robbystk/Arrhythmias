24ba71dfed11d9428704b8ec3556d612adb51456
Robby <robbystk@rams.colostate.edu>
2016-04-17 16:03:56 -0600

    description of QRS-finding function

---
+++ matlab/findqrs.m
@@ -0,0 +1,8 @@
+function peaks = findqrs(ecg)
+%findpeaks: finds the location of qrs complexes in an ecg using the
+%Pan-Tompkins algorithm
+%   usage:  peaks = findqrs(ecg);
+%   input:  ECG struct to find peaks in
+%   output: a list of indices of the peaks of the QRS complexes
+
+end % function
