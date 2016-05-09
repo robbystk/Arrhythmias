f423789c9f1687f09464f7f38f45979285dd2723
Robby <robbystk@rams.colostate.edu>
2016-05-08 20:10:52 -0600

    support variable crop widths and fix clipping

--- matlab/crop.m
+++ matlab/crop.m
@@ -1,28 +1,40 @@
-function excerpt = crop(ecg, n)
+function excerpt = crop(ecg, n, varargin)
 % crop:     extracts a portion of an ECG signal around the nth marker in
 %           the list of annotations
 %   usage:  excerpt = crop(ecg, n);
+%       or: excerpt = crop(ecg, n, before, after);
 %   input:  the ECG struct to be cropped
 %           the number of the beat to extract
+%           time in seconds before beat to begin excerpt (optional)
+%           time in seconds after beat to end excerpt (optional)
 %   output: an ECG struct with the extracted data, including the beat
 %           annotation
 
-if n > ecg.N
+if n > ecg.Nann
     error('crop: n is too big');
     return
 end % if
 
 % offsets
-before  = 0.300;    % time before beat to start excerpt in seconds
-after   = 0.500;    % time after beat to start excerpt in seconds
+switch nargin
+    case 2
+        before  = 0.300;    % time before beat to start excerpt in seconds
+        after   = 0.500;    % time after beat to start excerpt in seconds
+    case 4
+        before = varargin{1};
+        after = varargin{2};
+    otherwise
+        error('crop: must supply 2 or 4 arguments');
+        return
+end % switch
 
 % convert times to indices
 b_index = ecg.ann(n) - floor(before * ecg.fs);
 a_index = ecg.ann(n) + floor(after * ecg.fs);
 
 % clip indices within bounds
-if b_index < 0
-    b_index = 0;
+if b_index < 1
+    b_index = 1;
 end % if
 
 if a_index > ecg.N
