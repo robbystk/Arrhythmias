bc502bd65bebf7234b4ea654fe278d48478daba0
Robby <robbystk@rams.colostate.edu>
2016-05-12 18:18:13 -0600

    revert to original since beat selection can now be done by crop

--- matlab/plotecg.m
+++ matlab/plotecg.m
@@ -1,36 +1,22 @@
-function plotecg(ecg, varargin)
+function plotecg(ecg)
 % plotecg: plots data from ECG struct with annotations
 % marked
-%   usage:  plotecg(ecg) or plotecg(ecg,beats)
+%   usage:  plotecg(ecg)
 %   input:  the ecg struct to plot
-%           the beats to show (and annotate) (optional)
 %   output: none
 
     plot(ecg.time,ecg.signal); 
     title('ECG');
     xlabel('Time [s]');
     ylabel('Amplitude');
-    
-    switch nargin
-        case 1
-            beats = 1:ecg.Nann;
-            limits = [min(ecg.time) max(ecg.time)];
-        case 2
-            beats = varargin{1};
-            t_range = ecg.time(ecg.ann(varargin{1}));
-            limits = [max(min(t_range)-0.5,0), max(t_range) + 0.5];
-    end % switch
 
     hold on;
     ys = ylim();
     ty = ys(2) - (ys(2) - ys(1)) * 0.9;
-    for i = beats
+    for i = 1:ecg.Nann
         hold on;
         x = ecg.time(ecg.ann(i));
         line([x x],ys,'color','green');
     end % for
-    
-    xlim(limits)
-    
     hold off;
 end % function
